a6a7df9faa83d1f7fdbecc342f735649977836db
Farhan Syed <farhansyed11.3@gmail.com>
2017-04-23 02:40:05 -0700

    Initial Commit

--- CoreDataExample/DisplayTableViewController.swift
+++ CoreDataExample/DisplayTableViewController.swift
@@ -9,87 +9,104 @@
 import UIKit
 
 class DisplayTableViewController: UITableViewController {
-
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    var items: [Item] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        self.title = "My Entries"
+        
+        self.tableView.estimatedRowHeight = 44
+        self.tableView.rowHeight = UITableViewAutomaticDimension
+ 
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    
+    override func viewWillAppear(_ animated: Bool) {
+        fetchData()
+    }
+    
+    
+    func fetchData() {
+        
+        do {
+            items = try context.fetch(Item.fetchRequest())
+            print(items)
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        } catch {
+            print("Couldn't Fetch Data")
+        }
+        
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
+}
 
-    /*
+
+extension DisplayTableViewController {
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as UITableViewCell
+        
+        let date = items.reversed()[indexPath.row].date
+        let time = items.reversed()[indexPath.row].time
+        
+        cell.textLabel?.text = items.reversed()[indexPath.row].name
+        
+        if let date = date, let time = time {
+            let timeStamp = "Added on \(date) at \(time)"
+            cell.detailTextLabel?.text = timeStamp
 
-        // Configure the cell...
-
+        }
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return items.count
+        
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(items[indexPath.row].name!)
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        
+        let delete = UITableViewRowAction(style: .default, title: "Delete") { (action, indexPath) in
+            // delete item at indexPath
+            
+            let item = self.items[indexPath.row]
+            self.context.delete(item)
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+            
+            self.items.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            
+        }
+        
+        let share = UITableViewRowAction(style: .default, title: "Share") { (action, indexPath) in
+            // delete item at indexPath
+            
+            print("Share")
+            
+        }
+        
+        delete.backgroundColor = UIColor(red: 0/255, green: 177/255, blue: 106/255, alpha: 1.0)
+        share.backgroundColor = UIColor(red: 54/255, green: 215/255, blue: 183/255, alpha: 1.0)
+
+        
+        return [delete,share]
+        
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
--- CoreDataExample/addItemViewController.swift
+++ CoreDataExample/addItemViewController.swift
@@ -8,28 +8,85 @@
 
 import UIKit
 
-class addItemViewController: UIViewController {
-
+class addItemViewController: UIViewController, UITextViewDelegate {
+    
+    @IBOutlet var itemEntryTextView: UITextView?
+    
+    @IBAction func cancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    @IBAction func saveContact(_ sender: Any) {
+        
+        guard let enteredText = itemEntryTextView?.text else {
+            return
+        }
+        
+        if enteredText.isEmpty || itemEntryTextView?.text == "Type anything..."{
+            print("No Data")
+            
+            let alert = UIAlertController(title: "Please Type Something", message: "Your entry was left blank.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Okay", style: .default) { action in
+                
+            })
+            
+            self.present(alert, animated: true, completion: nil)
+            
+        } else {
+            
+            let date = Date()
+            let formatter = DateFormatter()
+            formatter.dateFormat = "MM/dd/YY"
+            let currentDate = formatter.string(from: date)
+            
+            let timeFormatter = DateFormatter()
+            timeFormatter.timeStyle = .short
+            let currentTime = timeFormatter.string(from: date)
+            
+            guard let entryText = itemEntryTextView?.text else {
+                return
+            }
+            
+            
+            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            let newEntry = Item(context: context)
+            newEntry.name = entryText
+            newEntry.date = currentDate
+            newEntry.time = currentTime
+            
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+            
+            dismiss(animated: true, completion: nil)
+            
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        itemEntryTextView?.delegate = self
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        textView.text = ""
+        textView.textColor = UIColor.black
     }
-    */
-
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if (text == "\n") {
+            textView.resignFirstResponder()
+            return false
+        }
+        return true
+    }
 }
+
+
